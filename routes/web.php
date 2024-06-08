0148850803d100d24a4fe8df3425132977635f91
raphaelazambuja <raphaelazambujadev@gmail.com>
2024-06-08 17:40:03 -0300

    fix: criada rota /

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CurriculumController;
 
+Route::get('/', [CurriculumController::class, 'index']);
 Route::resource('curriculum', CurriculumController::class);
